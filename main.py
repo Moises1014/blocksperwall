fd5a322cc16b647923d596dcceb34103d09bc0f9
mfeli040 <moises.feliz001@gmail.com>
2020-12-10 17:50:34 +0000

    Changed the output to csv file

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import csv
+import pandas as pd
 
+
+blocks_for_walls = []
+walls_items = []
 
 walls = input('Please enter a how many walls there are: ')
-blocks_for_walls = []
+
+def create_row(length_wall,height_wall,holes,area_holes,wall_area,length_block,height_block,blocks):
+  walls_item ={
+    'Length_of_Wall' : length_wall,
+    'Height_of_the_Wall' : height_wall,
+    'Number_of_Doors_windows_holes':holes,
+    'Combined_Areas_of_all_Doors_windows_holes':area_holes,
+    'Area_of_Wall' : wall_area,
+    'Legnth_of_the_blocks' : length_block,
+    'Heigth_of_the_blocks' : height_block,
+    'Blocks_needed_for_Wall' : blocks,
+    }
+  walls_items.append(walls_item)
 
 def Solid_wall():
   #length of wall
@@ -19,6 +36,7 @@ def Solid_wall():
   # Total blocks used in this wall
   blocks = wall_area / block_area
   print('\nTotal Blocks for this wall '+str(blocks))
+  create_row(length_solid_wall, height_solid_wall,'none','none',wall_area,length_block,height_block,blocks)
   return blocks
 
 def Other_wall():
@@ -46,6 +64,7 @@ def Other_wall():
   #number of blocks in this wall
   blocks = (wall_area - sum(holes_area) )/ block_area
   print('\nTotal Blocks for this wall '+str(blocks))
+  create_row(length_solid_wall, height_solid_wall,num_of_holes,sum(holes_area),wall_area,length_block,height_block,blocks)
   return blocks
 
 
@@ -63,3 +82,23 @@ for i in range(0,int(walls)):
 # adds up total number of blocks used in every wall
 totalblocks = sum(blocks_for_walls)
 print("\nTotal blocks needed in all walls is: \n"+str(totalblocks))
+
+#create csv file
+df = pd.DataFrame(walls_items)
+df.to_csv('EstimationofWalls.csv')
+''''
+df = pd.read_csv('EstimationofWalls.csv')
+
+fig = go.Figure(data=[go.Table(
+    header=dict(values=list(df.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[df.Length_of_Wall, df.Height_of_the_Wall, df.Number_of_Doors_windows_holes, df.Combined_Areas_of_all_Doors_windows_holes, df.Area_of_Wall, df.Legnth_of_the_blocks,df.Heigth_of_the_blocks,df.Blocks_needed_for_Wall],
+               fill_color='lavender',
+               align='left'))
+])
+
+
+
+fig.show()
+'''
